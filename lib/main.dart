cfc144fab4ae79c4a28f8a0ef75dc95e0a3c0098
Ahmet Meriç Savaş <ahmetmeric.svs@gmail.com>
2026-05-16 00:08:24 +0300

    Supabase bağlantısı main dart içerisine kuruldu

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'login_page.dart';
+import 'home_page.dart';
 
 void main() async {
+  // Flutter elementlerinin sorunsuz yüklenmesini sağlar
   WidgetsFlutterBinding.ensureInitialized();
 
+  // BURASI KRİTİK: Supabase bağlantısını başlatıyoruz
   await Supabase.initialize(
-    url: 'https://kykjolaanudirklgbbte.supabase.co',
+    url: 'https://kykjolaanudirklgbbte.supabase.co/rest/v1/',
     anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt5a2pvbGFhbnVkaXJrbGdiYnRlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg2MDc3NjEsImV4cCI6MjA5NDE4Mzc2MX0.TsM6hmRyf9MwHo6Y3hdPsVPbLnqvUC1sz1uXMrjznBU',
   );
 
@@ -19,7 +23,13 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Harcama ve Borç Takip',
-      theme: ThemeData(primarySwatch: Colors.blue),
+      debugShowCheckedModeBanner: false, // Sağ üstteki kırmızı şeridi kaldırır
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        useMaterial3: true, // Modern görünüm için
+      ),
+      // ÖDEV GEREKSİNİMİ: Uygulama açıldığında oturum kontrolü yapar.
+      // Eğer kullanıcı daha önce giriş yaptıysa doğrudan Ana Sayfa'ya, yapmadıysa Giriş Sayfası'na yönlendirir.
       home: Supabase.instance.client.auth.currentSession == null
           ? const LoginPage()
           : const HomePage(),
